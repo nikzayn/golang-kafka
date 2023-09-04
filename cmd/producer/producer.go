af96b430a829e07f706d93d6a92dc36c80587c27
nikzayn <nikhilvaidyar1997@gmail.com>
2023-09-04 20:00:25 +0530

    find by user id  helper function --added

--- cmd/producer/producer.go
+++ cmd/producer/producer.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"errors"
+	"golang-kafka/pkg/models"
+)
+
 const (
 	ProducerPort       = ":8080"
 	KafkaServerAddress = "localhost:9092"
 	KafkaTopic         = "notifications"
 )
+
+var ErrUserNotFoundInProducer = errors.New("user not found")
+
+func findUserByID(id int, users []models.User) (models.User, error) {
+	for _, user := range users {
+		if user.ID == id {
+			return user, nil
+		}
+	}
+	return models.User{}, ErrUserNotFoundInProducer
+}
